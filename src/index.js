facf952b0ccc1f769a938d20e7bbe78ad561ca04
nasti <AnastasiyaNovikovaG>
2018-09-28 20:41:32 +0300

    answer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+    let array =[];
+    if(str.length%2!=0) return false;
+
+    for (let i = 0; i < bracketsConfig.length ; i++) {
+        array.push(bracketsConfig[i][0]+bracketsConfig[i][1]);
+    }
+
+    for (let i = 0; i < array.length+1; i++) {
+        for (let j = 0; j < array.length; j++) {
+           while(str.includes(array[j])){
+               str=str.slice(0,str.indexOf(array[j])) +
+                   str.slice(str.indexOf(array[j])+2);
+           }
+        }
+    }
+
+    if (str.length > 0){
+        return false;
+    }
+    else return true;
 }
